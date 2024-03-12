db4fb722932b4b3d75200920fe509fe8a99ad34a
sumit <realsumitkumar@gmail.com>
2024-03-12 23:30:21 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,20 @@
+const heading = React.createElement("h1",
+    { id: "heading" },
+    "Hello from the React")
+
+const parent = React.createElement("div",
+    { id: "parent" },
+    [React.createElement("div",
+        { id: "child1" },
+        [React.createElement("h1", {}, "I am a H1 tag!"), React.createElement("h2", {}, "I am a H2 tag!")]),
+    React.createElement("div",
+        { id: "child2" },
+        [React.createElement("h1", {}, "I am a H1 tag!"), React.createElement("h2", {}, "I am a H2 tag!")])]
+)
+
+console.log(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent)
+
+
+// start from 2nd Video
---
+++ README.md
@@ -0,0 +1,1 @@
+#This is the notes of the Namaste React course!
